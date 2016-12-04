8ec86abf987230c055ea3ae2dce29db56813552b
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-12-04 13:39:28 +0200

    done function with permutations

--- FCC/Advanced Algorithm Scripting/NoRepeatsPlease.js
+++ FCC/Advanced Algorithm Scripting/NoRepeatsPlease.js
@@ -1,6 +1,6 @@
 function permAlone(str) {
     var arr = str.split('');
-    var output = [];
+    var permutations = [];
 
     function swap(pos1, pos2, array) {
         var temp = array[pos1];
@@ -9,26 +9,70 @@ function permAlone(str) {
     }
 
     function generateHeap(arr, n) {
-        var n = n || arr.length;
+        n = n || arr.length;
 
         if (n === 1) {
-            output.push(arr.slice());
+            permutations.push(arr.slice().join(''));
         } else {
             for (var i = 0; i < n - 1; ++i) {
                 generateHeap(arr, n - 1);
-
-                if (n % 2 === 0) {
-                    swap(i, n - 1, arr);
-                } else {
-                    swap(0, n - 1, arr);
-                }
+                swap(n % 2 === 0 ? i : 0, n - 1, arr);
             }
             generateHeap(arr, n - 1);
         }
     }
-
     generateHeap(arr);
-    return output;
+
+    var regex = /(.)\1{1,}/;
+
+    return permutations.filter(function(elt) {
+        return !regex.test(elt);
+    }).length;
 }
 
 console.log(permAlone('aab'));
+
+// Variant 2
+function permAlone2(str) {
+
+  // Create a regex to match repeated consecutive characters.
+  var regex = /(.)\1+/g;
+
+  // Split the string into an array of characters.
+  var arr = str.split('');
+  var permutations = [];
+  var tmp;
+
+  // Return 0 if str contains same character.
+  if (str.match(regex) !== null && str.match(regex)[0] === str) return 0;
+
+  // Function to swap variables' content.
+  function swap(index1, index2) {
+    tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+  }
+
+  // Generate arrays of permutations using the algorithm.
+  function generate(int) {
+    if (int === 1) {
+      // Make sure to join the characters as we create  the permutation arrays
+      permutations.push(arr.join(''));
+    } else {
+      for (var i = 0; i != int; ++i) {
+        generate(int - 1);
+        swap(int % 2 ? 0 : i, int - 1);
+      }
+    }
+  }
+
+  generate(arr.length);
+
+  // Filter the array of repeated permutations.
+  var filtered = permutations.filter(function(string) {
+    return !string.match(regex);
+  });
+
+  // Return how many have no repetitions.
+  return filtered.length;
+}
